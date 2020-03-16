5fc3cce41ae3ef7489ee14d8c695cf4d261c4a79
tv-dem <69cobak@mail.ru>
2020-03-16 22:42:30 +0300

    feat: complit task singolo DOM

---
+++ sctipt.js
@@ -0,0 +1,85 @@
+document.querySelectorAll('.block-arrow').forEach((arrow)=>{
+    arrow.addEventListener('click', ()=>{
+        document.querySelectorAll('.slide').forEach((slide)=>{
+            slide.classList.toggle('active');
+        })
+        document.querySelector('.slider').classList.toggle('active');
+    })
+})
+document.querySelector('.buttom-1').addEventListener('click', ()=>{
+    document.querySelector('.ver-phone-off').classList.toggle('phone-on');
+})
+document.querySelector('.buttom-2').addEventListener('click', ()=>{
+    document.querySelector('.hor-phone-off').classList.toggle('phone-on');
+})
+
+let imgs = document.querySelectorAll('.gallery img');
+imgs.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        if(item.classList.contains('active')){ item.classList.remove('active'); return;}
+        imgs.forEach((img)=>{
+            if(img.classList.contains('active')) img.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
+
+let a = document.querySelectorAll('header a');
+a.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        a.forEach((i)=>{
+            i.classList.remove('active');
+        });
+        item.classList.add('active');
+    })
+})
+let modal = document.querySelector('.modal')
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault()
+    modal.classList.add('active')
+    let subj = document.querySelector('input.subj').value;
+    subj = (!subj) ? 'Without subject' : `Subject: ${subj}`
+    document.querySelector('p.subj').innerHTML = subj;
+    let proj = document.querySelector('textarea.proj').value;
+    proj = (!proj) ? 'Without description  ' : `Description  : ${proj}`
+    document.querySelector('p.proj').innerHTML = proj;
+})
+
+document.querySelectorAll('.modal input').forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        modal.classList.remove('active')
+    })
+})
+
+let images = [
+    "assets/Project1.png",
+    "assets/Project2.png",
+    "assets/Project3.png",
+    "assets/Project4.png",
+    "assets/Project5.png",
+    "assets/Project6.png",
+    "assets/Project7.png",
+    "assets/Project8.png",
+    "assets/Project9.png",
+    "assets/Project10.png",
+    "assets/Project11.png",
+    "assets/Project12.png",
+];
+
+document.querySelectorAll('.portfolio li').forEach((item)=>{
+    item.addEventListener('click', function(){
+        if(!this.classList.contains('active')){
+            document.querySelectorAll('.portfolio li').forEach((ii)=>{
+                ii.classList.remove('active');
+            })
+            this.classList.add('active');
+            images = images.reverse();
+            document.querySelectorAll('.gallery img').forEach((img, i)=>{
+                img.src = images[i];
+                img.classList.remove('active')
+            })
+            
+        }
+    })
+})
